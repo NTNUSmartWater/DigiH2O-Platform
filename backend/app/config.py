58c315c0b3fae316fb79dd0cf6f71b13d218e7fa
Lam Van Nguyen <vanln@ntnu.no>
2025-10-30 09:56:02 +0100

    Fix configuration on NTNU machine

--- backend/app/config.py
+++ backend/app/config.py
@@ -3,7 +3,6 @@ from pathlib import Path
 from contextlib import asynccontextmanager
 from Functions import dataset_manager
 from dotenv import load_dotenv
-# from dask.distributed import Client, LocalCluster
 
 # ============== Root directory ================
 load_dotenv()
@@ -22,35 +21,10 @@ async def lifespan(app):
     app.state.PROJECT_DIR = None
     # WAQ model type
     app.state.waq_model = None
-    # # Generate Dassh cluster
-    # cluster = client = None
-    # try:
-    #     cluster = LocalCluster(n_workers=2, threads_per_worker=2, 
-    #         memory_limit='4GB', dashboard_address=":8787", silence_logs=False)
-    #     client = Client(cluster)
-    #     print(f"🚀 Dask cluster started with dashboard link: {cluster.dashboard_link}")
-    #     app.state.dask_client = client
-    # except Exception as e: 
-    #     print(f"⚠️ Failed to start Dask cluster: {e}")
-    #     app.state.dask_client = None
     # Dataset
     app.state.dataset_manager = dataset_manager.DatasetManager()
     yield
-    app.state.dataset_manager.close()
-
-    # try:
-    #     yield
-    # finally:
-    #     try:
-    #         if client:
-    #             client.close(timeout=5)
-    #         if cluster:
-    #             cluster.close(timeout=5)
-    #         print("🛑 Dask cluster closed cleanly")
-    #     except Exception as e:
-    #         print(f"⚠️ Dask cluster shutdown error: {e}")
-    # # Close datasets and clear memory
-    # try:
-    #     app.state.dataset_manager.close()
-    # except Exception as e:
-    #     print(f"⚠️ Dataset manager close error: {e}")
+    try:
+        app.state.dataset_manager.close()
+    except Exception as e:
+        print(f"⚠️ Dataset manager close error: {e}")
