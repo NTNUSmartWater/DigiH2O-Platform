eaa9f2d8ee2b66e1989e1ed8281ac2d2f6dc1861
Lam Van Nguyen <vanln@ntnu.no>
2025-11-20 22:32:36 +0100

    Testing Docker

--- backend/app/config.py
+++ backend/app/config.py
@@ -7,15 +7,19 @@ from dotenv import load_dotenv
 # ============== Root directory ================
 load_dotenv()
 BACKEND_DIR = Path(__file__).parent.parent       # backend/
-env_mode = os.getenv("ENV", "development")
-if env_mode == "development":
-    PROJECT_STATIC_ROOT = os.getenv("PROJECT_STATIC_ROOT")
-    STATIC_DIR_BACKEND = os.getenv("STATIC_DIR_BACKEND")
-    STATIC_DIR_FRONTEND = os.getenv("STATIC_DIR_FRONTEND")
-else:
-    PROJECT_STATIC_ROOT = "/app/Delft_Projects"
-    STATIC_DIR_BACKEND = "/app/static"
-    STATIC_DIR_FRONTEND = "/usr/share/nginx/html/static"
+# env_mode = os.getenv("ENV", "development")
+# if env_mode == "development":
+#     PROJECT_STATIC_ROOT = os.getenv("PROJECT_STATIC_ROOT")
+#     STATIC_DIR_BACKEND = os.getenv("STATIC_DIR_BACKEND")
+#     STATIC_DIR_FRONTEND = os.getenv("STATIC_DIR_FRONTEND")
+# else:
+#     PROJECT_STATIC_ROOT = "/app/Delft_Projects"
+#     STATIC_DIR_BACKEND = "/app/static"
+#     STATIC_DIR_FRONTEND = "/usr/share/nginx/html/static"
+
+PROJECT_STATIC_ROOT = os.getenv("PROJECT_STATIC_ROOT", "/app/Delft_Projects")
+STATIC_DIR_BACKEND = os.getenv("STATIC_DIR_BACKEND", "/app/static")
+STATIC_DIR_FRONTEND = os.getenv("STATIC_DIR_FRONTEND", "/usr/share/nginx/html/static")
 DELFT_PATH = os.getenv("DELFT3D_PATH")
 GRID_PATH = os.getenv('GRID_PATH')
 
