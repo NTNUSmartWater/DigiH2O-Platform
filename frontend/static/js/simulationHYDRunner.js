2cb7487398dc975abddf06f497483c627902e709
Lam-V-Nguyen <vanlnNTNU@gmail.com>
2025-12-17 00:18:22 +0100

    Working on logs

--- frontend/static/js/simulationHYDRunner.js
+++ frontend/static/js/simulationHYDRunner.js
@@ -7,7 +7,7 @@ const checkboxContainer = () => document.getElementById('checkbox-container');
 const showCheckbox = () => document.getElementById('show-checkbox');
 const textareaWrapper = () => document.getElementById('form-row-textarea');
 
-let ws = null, content = '', currentProject = null, height = 0, isRunning = false;
+let ws = null, content = '', currentProject = null, height = 0, isRunning = false, logInterval = null;
 
 async function sendQuery(functionName, content){
     const response = await fetch(`/${functionName}`, {
@@ -38,23 +38,27 @@ async function getProjectList(target){
 
 }
 
-function statusUpdate(info, barObject, textObject) {
-    if (info.startsWith("[ERROR]")) { 
-        alert(info.replace('[ERROR]','')); 
-        textObject().innerText = "Simulation finished unseccessfully.";
-        return false;
-    }
-    if (info.startsWith("[PROGRESS]")) {
-        // Update progress
-        const values = info.replace('[PROGRESS] ', '').trim().split('|');
-        const percent = parseFloat(values[0] || 0), time_used = values[1] || 'N/A', time_left = values[2] || 'N/A';
-        textObject().innerText = `Completed: ${percent}% [Used: ${time_used} → Left: ${time_left}]`;
-        barObject().value = percent;
-    }
-    if (info.startsWith("[POSTPROCESS]")) textObject().innerText = info.replace('[POSTPROCESS]','');
-    if (info.startsWith("[FINISHED]")) textObject().innerText = info.replace('[FINISHED]','');
-    return true;
-}
+// function statusUpdate(info, barObject, textObject) {
+//     if (info.startsWith("[ERROR]")) { 
+//         alert(info.replace('[ERROR]','')); 
+//         textObject().innerText = "Simulation finished unsuccessfully.";
+//         if (logInterval) { clearInterval(logInterval); logInterval = null; }
+//     }
+//     if (info.startsWith("[PROGRESS]")) {
+//         // Update progress
+//         const values = info.replace('[PROGRESS] ', '').trim().split('|');
+//         const percent = parseFloat(values[0] || 0), time_used = values[1] || 'N/A', time_left = values[2] || 'N/A';
+//         textObject().innerText = `Completed: ${percent}% [Used: ${time_used} → Left: ${time_left}]`;
+//         barObject().value = percent;
+//     }
+//     if (info.startsWith("[POSTPROCESS]")) {
+//         textObject().innerText = info.replace('[POSTPROCESS]',''); barObject().value = 100; 
+//     }
+//     if (info.startsWith("[FINISHED]")) {
+//         textObject().innerText = info.replace('[FINISHED]',''); isRunning = false; barObject().value = 100;
+//         if (logInterval) { clearInterval(logInterval); logInterval = null; }
+//     }
+// }
 
 function updateSelection(){
     getProjectList(projectSelector());
@@ -69,28 +73,26 @@ function updateSelection(){
         }
         checkboxContainer().style.display = 'block'; textareaWrapper().style.display = 'block';
         const statusRes = await sendQuery('check_sim_status_hyd', {projectName: projectName});
-        const protocol = window.location.protocol === "https:" ? "wss" : "ws";
         if (statusRes.status === "running") {
-            if (!ws || ws.readyState === WebSocket.CLOSED) {
-                ws = new WebSocket(`${protocol}://${window.location.host}/sim_progress_hyd/${projectName}`);
-                ws.onmessage = (event) => {
-                    if (!event.data.includes("[PROGRESS]")) { content += event.data + '\n';}
-                    infoArea().value = content;
-                    statusUpdate(event.data, progressbar, progressText);
-                };
+            const res = await fetch(`/sim_log_full/${projectName}`);
+            if (res.ok) {
+                const data = await res.json();
+                infoArea().value = data.content || '';
+                progressText().innerText = statusRes.complete || '';
+                progressbar().value = statusRes.progress || 0;
             }
             showCheckbox().checked = true;
         } else {
-            showCheckbox().checked = false;
-            progressText().innerText = "No simulation running."; progressbar().value = 0;
+            progressText().innerText = "No simulation running."; 
+            showCheckbox().checked = false; progressbar().value = 0;
         }
         checkboxUpdate();
     });
     // Run new simulation
     runBtn().addEventListener('click', async () => {
         let lastContent = '';
-        if (isRunning) return; isRunning = true;
-        currentProject = projectSelector().value;
+        if (isRunning) return; 
+        isRunning = true; currentProject = projectSelector().value;
         if (!currentProject) {alert('Please select a project.'); return;}
         // Check if simulation is running
         const statusRes = await sendQuery('check_sim_status_hyd', {projectName: currentProject});
@@ -105,25 +107,51 @@ function updateSelection(){
         if (start.status === "error") {alert(start.message); return;}
         content = ''; infoArea().value = ''; progressbar().value = 0;
         progressText().innerText = 'Start running hydrodynamic simulation...';
-        // Run hydrodynamics simulation
-        const protocol = window.location.protocol === "https:" ? "wss" : "ws";
-        if (ws) ws.close();
-        ws = new WebSocket(`${protocol}://${window.location.host}/sim_progress_hyd/${currentProject}`);
-        // Update logs every 3 seconds
-        setInterval(async () => {
-            try {
-                const res = await fetch(`/sim_temp_log/${currentProject}`);
-                if (!res.ok) return;
-                const data = await res.json();
-                const newContent = data.line;
-                if (newContent !== lastContent) {
-                    if (!newContent.includes("[PROGRESS]")) { content += newContent + '\n';}
-                    infoArea().value = content;
-                    statusUpdate(newContent, progressbar, progressText);
-                    if (newContent.includes("[FINISHED]")) isRunning = false;
-                    lastContent = newContent;
-                }
-            } catch (error) { console.error("Fetch log failed:", error); }
+        // // Run hydrodynamics simulation
+        // const protocol = window.location.protocol === "https:" ? "wss" : "ws";
+        // if (ws) ws.close();
+        // ws = new WebSocket(`${protocol}://${window.location.host}/sim_progress_hyd/${currentProject}`);
+        // Update logs every 5 seconds
+        logInterval = setInterval(async () => {
+            const res = await fetch(`/sim_log_tail/${currentProject}`);
+            if (!res.ok) return;
+            const data = await res.json();
+            for (const line of data.lines) {
+                infoArea().value += line + "\n";
+                // statusUpdate(line, progressbar, progressText);
+            }
+            const statusRes = await sendQuery('check_sim_status_hyd', {projectName: currentProject});
+            if (statusRes.status === "running") {
+                progressText().innerText = statusRes.complete || '';
+                progressbar().value = statusRes.progress || 0;
+            } else if (statusRes.status === "postprocessing") {
+                progressText().innerText = 'Reorganizing outputs. Please wait...'; progressbar().value = 100;
+            } else if (statusRes.status === "finished") {
+                progressText().innerText = 'Finished running hydrodynamic simulation.'; isRunning = false; progressbar().value = 100;
+                if (logInterval) { clearInterval(logInterval); logInterval = null; }
+            } else {
+                // alert(info.replace('[ERROR]','')); 
+                progressText().innerText = "Simulation finished unsuccessfully.";
+                if (logInterval) { clearInterval(logInterval); logInterval = null; }
+            }
+
+
+            // try {
+            //     const res = await fetch(`/sim_temp_log/${currentProject}`);
+            //     if (!res.ok) return;
+            //     const data = await res.json();
+            //     const newContent = data.content;
+            //     console.log('New content:', newContent);
+            //     if (newContent !== lastContent && newContent) {
+            //         if (!newContent.includes("[PROGRESS]")) { content += newContent + '\n';}
+            //         infoArea().value = content;
+            //         statusUpdate(newContent, progressbar, progressText);
+            //         lastContent = newContent;
+            //     }
+            // } catch (error) { 
+            //     console.error("Fetch log failed:", error); 
+            //     if (logInterval) { clearInterval(logInterval); logInterval = null; }
+            // }
         }, 3000);
     });
 }
